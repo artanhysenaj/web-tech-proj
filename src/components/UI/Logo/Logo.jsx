a5c6abc7ffd544fad9bf89ed3f53e2342b6d37fb
Artan-Hysenaj <artan.hysenaj@gmail.com>
2022-06-18 13:25:11 +0200

    Update Logo.jsx

--- src/components/UI/Logo/Logo.jsx
+++ src/components/UI/Logo/Logo.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 const Logo = (props) => {
+  const navigate = useNavigate();
   return (
-    <div className="shadow-[0_10px_60px_5px_rgba(0,0,0,0.3)] rounded-full w-12 sm:w-[70px]">
+    <div
+      onClick={() => navigate("/")}
+      className="shadow-[0_10px_60px_5px_rgba(0,0,0,0.3)] rounded-full w-12 sm:w-[70px] hover:cursor-pointer"
+    >
       <img className="w-full" src={logo} alt="Errday-snippets" />
     </div>
   );
